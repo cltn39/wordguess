8c471fd76e132d23d6dc62cd9f54b1a2f68c6369
Gravitation <chisong@ucdavis.edu>
2019-04-02 19:43:21 -0700

    js to do list

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,30 @@
-// TO Do: Use Key events to listen for letters that players type
+// WHAT TO TRACK
+// track wins
+// track loses
+// track list of  words to guess
+// the current word to guess
+// track wrong guesses
+// track the key, user input.
+
+// WHHAT TO DO
+// TO DO: Use Key events to listen for letters that players type
 
 // TO DO: Choose a word
 
-// TO Do:  restart the game
+// TO DO:  restart the game
+
+// TO DO: Check if key pressed is in the current word
+
+// TO DO: check if user won
+ 
+// TO DO: check if user lost
+
+// TO DO: display the wins
+
+// TO DO: display the losses
+
+// TO DO: display the wrong guesses
+
+// TO DO: display the current word
+
+// TO DO: display the guesses remaining
